fd1f258ebac862d9cca1f9821ff866584bf276b1
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-27 18:06:42 -0700

    installed bootstrap

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,5 +1,8 @@
-
 import './App.css';
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+
+
 
 import Home from './components/Home'
 import About from './components/About'
